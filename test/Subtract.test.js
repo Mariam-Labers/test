7e66bf1661f4c3ef886a0d3e90fd20344478d173
Mariam-Labers <69308145+Mariam-Labers@users.noreply.github.com>
2022-09-20 14:46:46 +0000

    Add files via upload

---
+++ test/Subtract.test.js
@@ -0,0 +1,8 @@
+const subtract = require("./substract")
+
+
+test("subtract two numbers", () => {
+    expect("subtract"(50, 24)
+    )
+        
+})
---
+++ test/sum.test.js
@@ -0,0 +1,7 @@
+const subtract = require("./Subtract")
+
+
+test("accurately add two numbers", () => {
+    expect(sum(67, 43)
+    ).toBe(110)
+})
